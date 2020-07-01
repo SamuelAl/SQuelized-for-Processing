3b22ab6811b92cce4ac594d968c1a284978086ea
Samuel Alarco Cantos <ALARCOCS@tcd.ie>
2020-07-01 14:14:17 +0100

    Added more type specifications in table construction function

--- src/samuelal/squelized/MySQLConnection.java
+++ src/samuelal/squelized/MySQLConnection.java
@@ -15,26 +15,16 @@ import java.time.format.*;
 import java.time.LocalDate;
 
 
-/**
- * This is a template class and can be used to start a new processing Library.
- * Make sure you rename this class as well as the name of the example package 'template' 
- * to your own Library naming convention.
- * 
- * (the tag example followed by the name of an example included in folder 'examples' will
- * automatically include the example in the javadoc.)
- *
- * @example Hello 
- */
 
 public class MySQLConnection{
-	
+
 	public final static String VERSION = "##library.prettyVersion##";
 	private String url, user, password;
 	private Connection connection;
 
 	/**
-	 * a Constructor, usually called in the setup() method in your sketch to
-	 * initialize and start the Library.
+	 * a Constructor, usually called in the setup() 
+	 * 
 	 * 
 	 */
 	public MySQLConnection(String user, String password, String url) {
@@ -42,10 +32,11 @@ public class MySQLConnection{
 		this.user = user;
 		this.password = password;
 		this.url = url;
-		
+
 		//Try connection parameters
 		try {
 			connection = DriverManager.getConnection(url, user, password);
+			System.out.println("Connected as " + user + " to " + url);
 			connection.close();
 		}
 		catch (SQLException e) {
@@ -53,75 +44,100 @@ public class MySQLConnection{
 			System.out.println(e.getMessage());
 		}
 	}
-	
-	
+
+
 	private void welcome() {
 		System.out.println("##library.name## ##library.prettyVersion## by ##author##");
 	}
+
+	
+	/*
+	 * runs SQL query through connection
+	 * and returns Table object with results
+	 * 
+	 * @param	String
+	 * @return	Table
+	 */
 	
 	public Table runQuery(String query)
 	{
 		Table queryResult = new Table();
 		try {
 			Connection connection = DriverManager.getConnection(url, user, password);
-	        Statement statement = connection.createStatement();
-	        ResultSet results = statement.executeQuery(query);
-	        
-	        //Build results table
-	        ResultSetMetaData resultsMetaData = results.getMetaData();
-	        int columnCount = resultsMetaData.getColumnCount();
-	        for (int i= 1; i <= columnCount; i++)
-	        {
-	          String columnTitle = resultsMetaData.getColumnName(i);          
-	          queryResult.addColumn(columnTitle);
-	        }
-	        
-	        // Date format to be used
-	        // In the future should add functionality to specify data format.
-	        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-	        DateTimeFormatter  timestampFormat = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
-	        while (results.next())
-	        {
-	          TableRow newRow = queryResult.addRow();
-	          for (int i = 0; i < columnCount; i++)
-	          {
-	            String columnTitle = resultsMetaData.getColumnName(i + 1);
-	            int typeNumber = resultsMetaData.getColumnType(i + 1);
-	            switch(typeNumber)
-	            {
-	              // Type String
-	            case Types.VARCHAR:
-	            case Types.CHAR:
-	            case Types.LONGVARCHAR:
-	              newRow.setString(columnTitle, results.getString(columnTitle));
-	              break;
-	              // Type Date
-	            case Types.DATE:
-	              String parsedDate = dateFormat.format(results.getDate(columnTitle).toLocalDate());
-	              newRow.setString(columnTitle, parsedDate);
-	              break;
-	              // Type Timestamps
-	            case Types.TIME:
-	            	String parsedTime = timestampFormat.format(results.getTime(columnTitle).toLocalTime());
-	            	newRow.setString(columnTitle, parsedTime);
-	            case Types.TIMESTAMP:
-	            	String parsedTimeStamp = timestampFormat.format(results.getTimestamp(columnTitle).toLocalDateTime());
-	            	newRow.setString(columnTitle, parsedTimeStamp);
-	              // Type float
-	            case Types.FLOAT:
-	            case Types.DOUBLE:
-	              newRow.setFloat(columnTitle, results.getFloat(columnTitle));
-	              break;
-	              // Type int
-	            case Types.INTEGER:
-	              newRow.setInt(columnTitle, results.getInt(columnTitle));
-	              break;
-	            default:
-	              newRow.setString(columnTitle, results.getString(columnTitle));
-	              break;
-	            }
-	          }
-	        }
+			Statement statement = connection.createStatement();
+			ResultSet results = statement.executeQuery(query);
+
+			//Build results table
+			ResultSetMetaData resultsMetaData = results.getMetaData();
+			int columnCount = resultsMetaData.getColumnCount();
+			for (int i= 1; i <= columnCount; i++) {
+			
+				String columnTitle = resultsMetaData.getColumnName(i);          
+				queryResult.addColumn(columnTitle);
+			}
+
+			// Date format to be used
+			// In the future should add functionality to specify data format.
+			DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			DateTimeFormatter  timestampFormat = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+			while (results.next()) {
+			
+				TableRow newRow = queryResult.addRow();
+				for (int i = 0; i < columnCount; i++) {
+				
+					String columnTitle = resultsMetaData.getColumnName(i + 1);
+					int typeNumber = resultsMetaData.getColumnType(i + 1);
+					switch(typeNumber)
+					{
+					// Type String
+					case Types.VARCHAR:
+					case Types.NVARCHAR:
+					case Types.CHAR:
+					case Types.NCHAR:
+					case Types.LONGVARCHAR:
+						newRow.setString(columnTitle, results.getString(columnTitle));
+						break;
+						// Type Date
+					case Types.DATE:
+						String parsedDate = dateFormat.format(results.getDate(columnTitle).toLocalDate());
+						newRow.setString(columnTitle, parsedDate);
+						break;
+						// Type Time
+					case Types.TIME:
+					case Types.TIME_WITH_TIMEZONE:
+						String parsedTime = timestampFormat.format(results.getTime(columnTitle).toLocalTime());
+						newRow.setString(columnTitle, parsedTime);
+						// Type Timestamps
+					case Types.TIMESTAMP:
+					case Types.TIMESTAMP_WITH_TIMEZONE:
+						String parsedTimeStamp = timestampFormat.format(results.getTimestamp(columnTitle).toLocalDateTime());
+						newRow.setString(columnTitle, parsedTimeStamp);
+						break;
+						// Type float
+					case Types.FLOAT:
+					case Types.REAL:
+						newRow.setFloat(columnTitle, results.getFloat(columnTitle));
+						break;
+						// Type double
+					case Types.DOUBLE:
+						newRow.setDouble(columnTitle, results.getDouble(columnTitle));
+						break;
+						// Type int
+					case Types.INTEGER:
+					case Types.SMALLINT:
+					case Types.TINYINT:
+						newRow.setInt(columnTitle, results.getInt(columnTitle));
+						break;
+						// Type long
+					case Types.BIGINT:
+						newRow.setLong(columnTitle, results.getLong(columnTitle));
+						break;
+					default:
+						newRow.setString(columnTitle, results.getString(columnTitle));
+						break;
+					}
+				}
+			}
 		}
 		catch (SQLException e) {
 			System.out.println("Connection failed");
@@ -129,7 +145,7 @@ public class MySQLConnection{
 		} 
 		return queryResult;
 	}
-	
+
 	/**
 	 * return the version of the Library.
 	 * 
